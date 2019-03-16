e7a7d15db46178f9c94205a4e675f212c41a5129
Rojin Pourkhomami <Rojin@rojins-mbp.home>
2019-03-15 22:43:10 -0400

    updated file

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,10 @@
 var mysql = require('mysql');
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		port: 8080,
 		host: 'localhost',
 		user: 'root',
 		password: '',
